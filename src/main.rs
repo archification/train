e625164d178e7fa162eb22de750e6ec9df9a1c20
archification <archification@gmail.com>
2020-06-01 01:49:43 -0500

    stuff

--- src/main.rs
+++ src/main.rs
@@ -2,16 +2,11 @@ extern crate rand;
 
 use std::thread::spawn;
 
-mod lib;
 mod cube;
 
-use lib::make_danger;
-use lib::execute_danger;
 use cube::cube;
 
 fn main() -> Result<(), String> {
-    make_danger();
-    execute_danger();
     loop {
         spawn(cube).join().expect("thread panicked")?;
     }
